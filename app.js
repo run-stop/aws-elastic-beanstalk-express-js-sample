9f9c63b3d2773c69d3eb9b8f5653da3ce357ae89
run-stop <aleksandar.miljkovic@gmail.com>
2021-09-13 19:20:14 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Beautiful World!'));
+app.get('/', (req, res) => res.send('Hello Beautiful AWS World!<br/>Full Pipline Test!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
